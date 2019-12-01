8fe7ca153334ae0a37336bf4d2e901e5df4a7da6
Laptop <s6604582@stud.uni-frankfurt.de>
2019-12-01 22:20:00 +0100

    Update JavaDoc

--- src/com/therazzerapp/bibformatter/content/saver/BibSaver.java
+++ src/com/therazzerapp/bibformatter/content/saver/BibSaver.java
@@ -6,7 +6,7 @@ import com.therazzerapp.bibformatter.bibliographie.Bibliography;
 import com.therazzerapp.bibformatter.bibliographie.Entry;
 
 /**
- * <description>
+ * Manages the {@link Bibliography} exportation.
  *
  * @author Paul Eduard Koenig <s6604582@stud.uni-frankfurt.de>
  * @since 0.0.0
@@ -14,19 +14,19 @@ import com.therazzerapp.bibformatter.bibliographie.Entry;
 public class BibSaver {
 
     /**
-     *
-     * @param bibliography
-     * @param fileName
+     * Exports a {@link Bibliography} object as a .bib file.
+     * @param bibliography the bib to export
+     * @param fileName the path where to save the bib
      */
     public static void save(Bibliography bibliography, String fileName){
         StringBuilder sb = new StringBuilder();
         if (bibliography.getComments() != null){
             for (String s : bibliography.getComments()) {
-                sb.append(s + "\n");
+                sb.append(s).append("\n");
             }
         }
         sb.append("%%Modified using BibFormatter " + Constants.VERSION + "\n");
-        sb.append("%%Entries: " + bibliography.getEntrieList().size()+ "\n\n");
+        sb.append("%%Entries: ").append(bibliography.getEntrieList().size()).append("\n\n");
         for (Entry entry : bibliography.getEntrieList()) {
             sb.append(entry.getRawEntry());
         }
